cf068ddafcc0bd73240eff98eb22bd0517f40e7c
MPoleto <poleto.mari@gmail.com>
2022-11-03 11:17:45 -0300

    done

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+Random random = new Random();
+int numeroCapacidade = random.Next(1, 5);
+int numeroDias = random.Next(1, 15);
+
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -12,14 +15,14 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 hospedes.Add(p1);
 hospedes.Add(p2);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: numeroCapacidade, valorDiaria: 30);
+
+
+Reserva reserva = new Reserva(diasReservados: numeroDias);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
+
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Valor total: {reserva.CalcularValorDiaria():C} para {numeroDias} dia(s).");
